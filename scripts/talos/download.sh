b9eec3a9ea84ad4fe41b1f3642609be22d0594e2
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2024-08-17 09:25:31 +0200

    fix PR changes

--- scripts/talos/download.sh
+++ scripts/talos/download.sh
@@ -2,9 +2,11 @@
 
 PROJECT_ROOT=$(readlink -e "$(dirname $0)/../..")
 TALOS_MEDIA_PATH="$PROJECT_ROOT/terraform/infra/talos"
+# renovate: datasource=docker depName=ghcr.io/siderolabs/installer
+TALOS_VERSION="v1.7.5"
 
 if [[ ! -d $TALOS_MEDIA_PATH ]] then
     mkdir $TALOS_MEDIA_PATH
 fi
 
-curl -L "https://github.com/siderolabs/talos/releases/download/v1.7.5/nocloud-amd64.raw.xz" | xz -d > "$TALOS_MEDIA_PATH/nocloud-amd64.raw"
+curl -L "https://github.com/siderolabs/talos/releases/download/$TALOS_VERSION/nocloud-amd64.raw.xz" | xz -d > "$TALOS_MEDIA_PATH/nocloud-amd64.raw"
