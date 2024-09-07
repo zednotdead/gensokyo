6e6fa1ad63a670e8b57ed738929db69cccb31a18
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2024-09-08 01:08:01 +0200

    change collation

--- terraform/authentik/applications.tf
+++ terraform/authentik/applications.tf
@@ -19,27 +19,6 @@ resource "authentik_application" "grafana_application" {
   policy_engine_mode = "all"
 }
 
-resource "authentik_provider_oauth2" "tandoor_oauth2" {
-  name                  = "tandoor"
-  client_id             = local.tandoor_id
-  client_secret         = local.tandoor_secret
-  authorization_flow    = resource.authentik_flow.provider-authorization-implicit-consent.uuid
-  property_mappings     = data.authentik_scope_mapping.oauth2.ids
-  access_token_validity = "hours=4"
-  redirect_uris         = ["https://tandoor.${var.cluster_domain}/accounts/oidc/authentik/login/callback/"]
-}
-
-resource "authentik_application" "tandoor_application" {
-  name               = "Tandoor"
-  slug               = authentik_provider_oauth2.tandoor_oauth2.name
-  protocol_provider  = authentik_provider_oauth2.tandoor_oauth2.id
-  group              = authentik_group.users.name
-  open_in_new_tab    = true
-  meta_icon          = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/png/tandoor.png"
-  meta_launch_url    = "https://tandoor.${var.cluster_domain}"
-  policy_engine_mode = "all"
-}
-
 resource "authentik_provider_oauth2" "mealie_oauth2" {
   name                  = "mealie"
   client_type           = "public"
@@ -61,5 +40,26 @@ resource "authentik_application" "mealie_application" {
   open_in_new_tab    = true
   meta_icon          = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/png/mealie.png"
   meta_launch_url    = "https://mealie.${var.cluster_domain}"
+  policy_engine_mode = "all"
+}
+
+resource "authentik_provider_oauth2" "synapse_oauth2" {
+  name                  = "synapse"
+  client_id             = local.synapse_id
+  client_secret         = local.synapse_secret
+  authorization_flow    = resource.authentik_flow.provider-authorization-implicit-consent.uuid
+  property_mappings     = data.authentik_scope_mapping.oauth2.ids
+  access_token_validity = "hours=4"
+  redirect_uris         = ["https://matrix.${var.cluster_domain}/_synapse/client/oidc/callback"]
+}
+
+resource "authentik_application" "synapse_application" {
+  name               = "Synapse"
+  slug               = authentik_provider_oauth2.synapse_oauth2.name
+  protocol_provider  = authentik_provider_oauth2.synapse_oauth2.id
+  group              = authentik_group.users.name
+  open_in_new_tab    = true
+  meta_icon          = "https://matrix.org/images/matrix-logo-white.svg"
+  meta_launch_url    = "https://matrix.${var.cluster_domain}"
   policy_engine_mode = "all"
 }
