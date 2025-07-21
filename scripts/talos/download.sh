2a71a410fc66ed33a5a3302e4f375460985c3cea
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2025-07-21 19:19:23 +0200

    test

--- scripts/talos/download.sh
+++ scripts/talos/download.sh
@@ -4,7 +4,7 @@ PROJECT_ROOT=$(readlink -e "$(dirname $0)/../..")
 TALOS_MEDIA_PATH="$PROJECT_ROOT/terraform/infra/talos"
 # renovate: datasource=docker depName=ghcr.io/siderolabs/installer
 TALOS_VERSION="v1.10.5"
-TALOS_SCHEMATIC_ID="c922b1884dbba1e8df59bc0d971cf78a47af3487863a4e08c9ecbc5158d4a8d6"
+TALOS_SCHEMATIC_ID="06e6f5ace9d08b7595be32a03d6d779b2149404e8a5d32011c0b02a1f36b0499"
 
 if [[ ! -d $TALOS_MEDIA_PATH ]] then
     mkdir $TALOS_MEDIA_PATH
